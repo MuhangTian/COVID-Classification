899078d52ea5d27e77a6af6433584acbb23a5c5f
Tony_Tian_1122 <tonytian@stayafloat.io>
2023-01-02 02:50:39 -0500

    complete most of the model, still need to fix todo

--- models/EfficientDet.py
+++ models/EfficientDet.py
@@ -1,7 +1,296 @@
-from effdet.config.model_config import efficientdet_model_param_dict
-from effdet import get_efficientdet_config, EfficientDet, DetBenchTrain
-from effdet.efficientdet import HeadNet
-from effdet.config.model_config import efficientdet_model_param_dict
-import timm
+from utils.model_helper import create_model, run_wbf
+from utils.image_helper import DataAdapter
+from torch.utils.data import Dataset
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import DataLoader
+from fastcore.dispatch import typedispatch
+from typing import List
+import pytorch_lightning as pl
+import torch
+import numpy as np
+import pandas as pd
 
-print(efficientdet_model_param_dict.keys())
+class EfficientDetDataset(Dataset):
+    """
+    Hold dataset using adapter in order to use PyTorch
+
+    Args:
+        da : DataAdapter
+        transforms: function which applies data transform (e.g. train_trainsform)
+    """
+    def __init__(self, da, transforms):
+        self.da = da
+        self.transforms = transforms
+
+    def __getitem__(self, idx):
+        (name, img, bbox, label) = self.da.i_get(idx)
+        sample = {
+            "image": np.array(img, dtype=np.float32),
+            "bboxes": bbox,
+            "class_labels": label,
+        }
+
+        transformed = self.transforms(**sample) # Apply data augmentation and transformation
+        img = transformed["image"]  # get transformed image
+        transformed['bboxes'] = np.array(transformed["bboxes"])
+        label = transformed["class_labels"]
+
+        _, height, width = img.shape    # the first is channel (depth), not needed
+        transformed["bboxes"][:, [0, 1, 2, 3]] = transformed["bboxes"][:, [1, 0, 3, 2]]  # convert to yxyx
+        target = {
+            "bboxes": torch.as_tensor(transformed["bboxes"], dtype=torch.float32),
+            "labels": torch.as_tensor(label),
+            "image_id": torch.tensor([name]),
+            "img_size": (height, width),
+            "img_scale": torch.tensor([1.0]),
+        }
+
+        return img, target, name
+
+    def __len__(self): return len(self.da)
+
+class EfficientDetDataModule(LightningDataModule):
+    ''' https://pytorch-lightning.readthedocs.io/en/stable/data/datamodule.html '''
+    def __init__(self, df, frac, train_transforms,
+                 val_transforms, num_workers=4, batch_size=8):
+        df_train = df.sample(frac=frac)
+        df_val = df.drop(df_train.index)
+        self.train_da = DataAdapter(df_train)
+        self.val_da = DataAdapter(df_val)
+        self.train_tfms = train_transforms
+        self.val_tfms = val_transforms
+        self.num_workers = num_workers
+        self.batch_size = batch_size
+        super().__init__()
+
+    def train_dataloader(self) -> DataLoader:
+        train_dataset = EfficientDetDataset(da=self.train_da, transforms=self.train_tfms)
+        train_loader = DataLoader(
+            train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            pin_memory=True,
+            drop_last=True,
+            num_workers=self.num_workers,
+            collate_fn=self.collate_fn,
+        )
+
+        return train_loader
+
+    def val_dataloader(self) -> DataLoader:
+        val_dataset = EfficientDetDataset(da=self.val_da, transforms=self.val_tfms)
+        val_loader = DataLoader(
+            val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            pin_memory=True,
+            drop_last=True,
+            num_workers=self.num_workers,
+            collate_fn=self.collate_fn,
+        )
+
+        return val_loader
+    
+    @staticmethod
+    def collate_fn(batch):
+        images, targets, image_ids = tuple(zip(*batch))
+        images = torch.stack(images)
+        images = images.float()
+
+        boxes = [target["bboxes"].float() for target in targets]
+        labels = [target["labels"].float() for target in targets]
+        img_size = torch.tensor([target["img_size"] for target in targets]).float()
+        img_scale = torch.tensor([target["img_scale"] for target in targets]).float()
+
+        annotations = {
+            "bbox": boxes,
+            "cls": labels,
+            "img_size": img_size,
+            "img_scale": img_scale,
+        }
+
+        return images, annotations, targets, image_ids
+
+class EfficientDetModel(pl.LightningModule):
+    def __init__(
+        self,
+        inference_transforms,
+        backbone,
+        num_classes=2,
+        img_size=512,
+        predict_confidence_thres=0.2,
+        lr=0.0002,
+        wbf_thres=0.44,
+    ):
+        super().__init__()
+        self.img_size = img_size
+        self.model = create_model(num_classes, img_size, backbone)
+        self.predict_confidence_thres = predict_confidence_thres
+        self.lr = lr
+        self.wbf_thres = wbf_thres
+        self.inference_tfms = inference_transforms
+
+    def forward(self, images, targets):
+        return self.model(images, targets)
+
+    def configure_optimizers(self): # TODO: may need to consider other optimizers
+        return torch.optim.AdamW(self.model.parameters(), lr=self.lr)
+    
+    def training_step(self, batch, batch_idx):
+        images, annotations, _, image_ids = batch
+        losses = self.model(images, annotations)
+        # TODO: need to find some performance metric to be stored in log
+        log_losses = {
+            "class_loss": losses["class_loss"].detach(),
+            "box_loss": losses["box_loss"].detach(),
+        }
+        self.log('Train Loss', losses['loss'])
+        self.log('Train Classification Loss', log_losses['class_loss'])
+        self.log('Train Localization Loss', log_losses['box_loss'])
+
+        return losses['loss']
+    
+    def training_epoch_end(self, outputs: EPOCH_OUTPUT) -> None:
+        return super().training_epoch_end(outputs)
+    
+    @torch.no_grad()    # disable training since currently in validation
+    def validation_step(self, batch, batch_idx):
+        images, annotations, targets, image_ids = batch
+        outputs = self.model(images, annotations)
+        detections = outputs["detections"]
+        # TODO: need to find some performance metric to be stored in log
+        batch_predictions = {
+            "predictions": detections,
+            "targets": targets,
+            "image_ids": image_ids,
+        }
+
+        log_loss = {
+            "class_loss": outputs["class_loss"].detach(),
+            "box_loss": outputs["box_loss"].detach(),
+        }        
+        self.log('Validation Loss', outputs["loss"])
+        self.log('Validation Classification Loss', log_loss["class_loss"])
+        self.log('Validation Localization Loss', log_loss["box_loss"])
+
+        return {'loss': outputs["loss"], 'batch_predictions': batch_predictions}
+    
+    @typedispatch
+    def predict(self, images: List):
+        """
+        For making predictions from images
+        Args:
+            images: a list of PIL images
+
+        Returns: a tuple of lists containing bboxes, predicted_class_labels, predicted_class_confidences
+        """
+        image_sizes = [(image.size[1], image.size[0]) for image in images]
+        images_tensor = torch.stack(
+            [
+                self.inference_tfms(    # CHECK: class and bboxes is fixed 
+                    image=np.array(image, dtype=np.float32),
+                    class_labels=np.ones(1),
+                    bboxes=np.array([[0, 0, 1, 1]]),
+                )["image"]
+                for image in images
+            ]
+        )
+
+        return self._run_inference(images_tensor, image_sizes)
+    
+    @typedispatch
+    def predict(self, images_tensor: torch.Tensor):
+        """
+        For making predictions from tensors returned from the model's dataloader
+        Args:
+            images_tensor: the images tensor returned from the dataloader
+
+        Returns: a tuple of lists containing bboxes, predicted_class_labels, predicted_class_confidences
+        """
+        if images_tensor.ndim == 3:
+            images_tensor = images_tensor.unsqueeze(0)
+        if images_tensor.shape[-1] != self.img_size or images_tensor.shape[-2] != self.img_size:
+            raise ValueError(f"Input tensors must be of shape (N, 3, {self.img_size}, {self.img_size})")
+
+        num_images = images_tensor.shape[0]
+        image_sizes = [(self.img_size, self.img_size)] * num_images
+
+        return self._run_inference(images_tensor, image_sizes)
+    
+    def _run_inference(self, images_tensor, image_sizes):
+        dummy_targets = self._create_dummy_inference_targets(num_images=images_tensor.shape[0])
+        detections = self.model(images_tensor.to(self.device), dummy_targets)["detections"]
+        (
+            predicted_bboxes,
+            predicted_class_confidences,
+            predicted_class_labels,
+        ) = self.post_process_detections(detections)
+
+        scaled_bboxes = self.__rescale_bboxes(
+            predicted_bboxes=predicted_bboxes, image_sizes=image_sizes
+        )
+
+        return scaled_bboxes, predicted_class_labels, predicted_class_confidences
+    
+    def _create_dummy_inference_targets(self, num_images):
+        dummy_targets = {
+            "bbox": [
+                torch.tensor([[0.0, 0.0, 0.0, 0.0]], device=self.device)
+                for i in range(num_images)
+            ],
+            "cls": [torch.tensor([1.0], device=self.device) for i in range(num_images)],
+            "img_size": torch.tensor(
+                [(self.img_size, self.img_size)] * num_images, device=self.device
+            ).float(),
+            "img_scale": torch.ones(num_images, device=self.device).float(),
+        }
+
+        return dummy_targets
+    
+    def post_process_detections(self, detections):
+        predictions = []
+        for i in range(detections.shape[0]):
+            predictions.append(
+                self._postprocess_single_prediction_detections(detections[i])
+            )
+
+        predicted_bboxes, predicted_class_confidences, predicted_class_labels = run_wbf(
+            predictions, image_size=self.img_size, iou_thr=self.wbf_iou_threshold
+        )
+
+        return predicted_bboxes, predicted_class_confidences, predicted_class_labels
+    
+    def _postprocess_single_prediction_detections(self, detections):
+        boxes = detections.detach().cpu().numpy()[:, :4]
+        scores = detections.detach().cpu().numpy()[:, 4]
+        classes = detections.detach().cpu().numpy()[:, 5]
+        indexes = np.where(scores > self.prediction_confidence_threshold)[0]
+        boxes = boxes[indexes]
+
+        return {"boxes": boxes, "scores": scores[indexes], "classes": classes[indexes]}
+    
+    def __rescale_bboxes(self, predicted_bboxes, image_sizes):
+        scaled_bboxes = []
+        for bboxes, img_dims in zip(predicted_bboxes, image_sizes):
+            im_h, im_w = img_dims
+
+            if len(bboxes) > 0:
+                scaled_bboxes.append(
+                    (
+                        np.array(bboxes)
+                        * [
+                            im_w / self.img_size,
+                            im_h / self.img_size,
+                            im_w / self.img_size,
+                            im_h / self.img_size,
+                        ]
+                    ).tolist()
+                )
+            else:
+                scaled_bboxes.append(bboxes)
+
+        return scaled_bboxes
+    
+
+if __name__ == '__main__':
+    print(type(DataAdapter))
