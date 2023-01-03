6bd0fd57a072acea9ed21a351a6267f3b2815de6
Tony_Tian_1122 <tonytian@stayafloat.io>
2023-01-03 02:06:48 -0500

    tc

--- effdet.sh
+++ effdet.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=EffDetD6
+#SBATCH --job-name=EffDet
 #SBATCH --time=30-00:00:00
 #SBATCH -n 1
-#SBATCH --nodelist=gpu-compute5
-#SBATCH --gpus-per-task=8
+#SBATCH --gpus-per-task=4
 #SBATCH --mem=100G
 #SBATCH --mail-user=muhang.tian@duke.edu
-#SBATCH --output=EffDetD6.out
+#SBATCH --output=EffDet.out
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
-srun python3 -u run.py -wdb True -p 'config/tf_efficientdet_d6.yaml'
+srun python3 -u run.py -wdb True -p 'config/tf_efficientdet_d4.yaml'
 wait
