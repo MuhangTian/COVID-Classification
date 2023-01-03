bd235442715e5271647a4e30dc962afda29debce
Tony_Tian_1122 <tonytian@stayafloat.io>
2023-01-02 19:11:09 -0500

    tc

--- effdet.sh
+++ effdet.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=30-00:00:00
 #SBATCH -n 1
 #SBATCH --gpus-per-task=5
-#SBATCH --mem=100G
+#SBATCH --mem=50G
 #SBATCH --mail-user=muhang.tian@duke.edu
 #SBATCH --output=None
 #SBATCH --mail-type=END
