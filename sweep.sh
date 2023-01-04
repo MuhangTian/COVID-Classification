8fcaf1e062139853afda355b1950894df923103b
Tony_Tian_1122 <tonytian@stayafloat.io>
2023-01-04 17:38:25 -0500

    tc

--- sweep.sh
+++ sweep.sh
@@ -9,5 +9,5 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
-srun wandb agent "muhang-tian/EfficientDetV2 DS Sweep/40w0285y"
+srun wandb agent "muhang-tian/EfficientDetV2 DS Sweep/opnlap5u"
 wait
