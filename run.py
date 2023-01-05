ecb2ebf3ddc3850a2b180d6fdb896b8da571f552
Tony_Tian_1122 <tonytian@stayafloat.io>
2023-01-04 22:43:00 -0500

    tc

--- run.py
+++ run.py
@@ -75,6 +75,8 @@ def run(config, wdb, mode):
                     val_check_interval=1,
                     num_sanity_val_steps=1,
                     log_every_n_steps=50,
+                    strategy='dp',      # NOTE: for GPUs in cluster
+                    devices=10,         # NOTE: for GPUS in cluster
     )
     
     trainer.fit(model, datamodule=module)
--- sweep.sh
+++ sweep.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=SP-EffDet
 #SBATCH --time=60-00:00:00
 #SBATCH -n 1
-#SBATCH --gpus-per-task=4
-#SBATCH --mem=100G
+#SBATCH --gpus-per-task=10
+#SBATCH --mem=400G
 #SBATCH --mail-user=muhang.tian@duke.edu
 #SBATCH --output=None
 #SBATCH --mail-type=END
