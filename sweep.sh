f12b2bd543af4a9247fb21daa9aa2eb586746579
Tony_Tian_1122 <tonytian@stayafloat.io>
2023-01-04 22:44:51 -0500

    tc

--- sweep.sh
+++ sweep.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=SP-EffDet
 #SBATCH --time=60-00:00:00
 #SBATCH -n 1
-#SBATCH --gpus-per-task=8
-#SBATCH --mem=100G
+#SBATCH --gpus-per-task=10
+#SBATCH --mem=50G
 #SBATCH --mail-user=muhang.tian@duke.edu
 #SBATCH --output=None
 #SBATCH --mail-type=END
