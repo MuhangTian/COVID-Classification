ced02c44eb18c278fd0eb34b4dba0e0b0e53fa5e
Tony_Tian_1122 <tonytian@stayafloat.io>
2023-01-05 00:29:36 -0500

    tc

--- sweep.sh
+++ sweep.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 #SBATCH --job-name=SP-EffDet
 #SBATCH --time=90-00:00:00
-#SBATCH -n 1
+#SBATCH -n 8
 #SBATCH --gpus-per-task=4
-#SBATCH --mem=100G
+#SBATCH --mem-per-gpu=20G
 #SBATCH --mail-user=muhang.tian@duke.edu
 #SBATCH --output=None
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
-srun wandb agent "muhang-tian/EfficientDet D0 Sweep/96eciwyc"
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/8evzwxia" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/8evzwxia" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/8evzwxia" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/8evzwxia" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/8evzwxia" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/8evzwxia" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/8evzwxia" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/8evzwxia" &
 wait
