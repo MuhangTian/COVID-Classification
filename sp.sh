1be5d54d99ad38aed39107dcfce95a88875dff2c
Tony_Tian_1122 <tonytian@stayafloat.io>
2023-01-05 02:19:42 -0500

    tc

--- sp.sh
+++ sp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=SP-EffDet
 #SBATCH --time=90-00:00:00
-#SBATCH -n 1
+#SBATCH -n 15
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-gpu=50G
 #SBATCH --mail-user=muhang.tian@duke.edu
@@ -9,4 +9,21 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
-srun wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi"
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi" &
+
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi" &
+
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDet D0 Sweep/0tzeqidi" &
+wait
