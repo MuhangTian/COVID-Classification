3d21799960ce85203e008388abe6c9e7c25a39bd
Tony_Tian_1122 <tonytian@stayafloat.io>
2023-01-06 01:48:10 -0500

    tc

--- sp.sh
+++ sp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=SP-EffDet
 #SBATCH --time=90-00:00:00
-#SBATCH -n 10
+#SBATCH -n 20
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-gpu=50G
 #SBATCH --mail-user=muhang.tian@duke.edu
@@ -11,6 +11,18 @@
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate covid-cv
+
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/m77pgogj" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/m77pgogj" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/m77pgogj" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/m77pgogj" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/m77pgogj" &
+
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/m77pgogj" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/m77pgogj" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/m77pgogj" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/m77pgogj" &
+srun --exclusive -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/m77pgogj" &
 
 srun --exclusive -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/m77pgogj" &
 srun --exclusive -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/m77pgogj" &
