46e3ae494fad1318bba6a75cb6843edce9e0f6c2
Tony_Tian_1122 <tonytian@stayafloat.io>
2023-01-06 01:18:02 -0500

    tc

--- sp.sh
+++ sp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=SP-EffDet
 #SBATCH --time=90-00:00:00
-#SBATCH -n 1
+#SBATCH --nodes=20
+#SBATCH -n 20
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-gpu=40G
 #SBATCH --mail-user=muhang.tian@duke.edu
@@ -9,4 +10,27 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
-srun wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n"
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+srun --exclusive --nodes=1 -n1 wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/fkjepv2n" &
+wait
