3e4a0736b46962365e37ee777874aa5769c93fe7
Tony_Tian_1122 <tonytian@stayafloat.io>
2023-01-06 00:09:29 -0500

    tc

--- sp.sh
+++ sp.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=SP-EffDet
 #SBATCH --time=90-00:00:00
 #SBATCH -n 1
-#SBATCH --gpus-per-task=4
+#SBATCH --gpus-per-task=3
 #SBATCH --mem-per-gpu=10G
 #SBATCH --mail-user=muhang.tian@duke.edu
 #SBATCH --output=None
