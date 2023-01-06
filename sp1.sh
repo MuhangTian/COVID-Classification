28e9b5c94a4624edf30a6238f769bfc3b2c8ce40
Tony_Tian_1122 <tonytian@stayafloat.io>
2023-01-06 02:44:48 -0500

    tc

--- sp1.sh
+++ sp1.sh
@@ -17,4 +17,4 @@ echo "SLURM_JOBID: " $SLURM_JOBID
 echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID 
 echo "SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
 
-srun $SLURM_ARRAY_TASK_ID wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/m77pgogj"
+srun wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/m77pgogj"
