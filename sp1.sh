da1d0fbc98ea98114d9bf0a56f9e8c96a267dc44
Tony_Tian_1122 <tonytian@stayafloat.io>
2023-01-06 02:39:25 -0500

    tc

---
+++ sp1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=SP-EffDet
+#SBATCH --time=90-00:00:00
+#SBATCH --array=1-20
+#SBATCH --gpus-per-task=1
+#SBATCH --mem-per-gpu=30G
+#SBATCH --mail-user=muhang.tian@duke.edu
+#SBATCH --output=None
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate covid-cv
+
+echo "SLURM_JOBID: " $SLURM_JOBID
+echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID 
+echo "SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
+
+srun wandb agent "muhang-tian/EfficientDetD0 Sweep (Random)/m77pgogj" $SLURM_ARRAY_TASK_ID
